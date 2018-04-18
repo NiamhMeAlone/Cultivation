26dc1172f8c19cde16e785c558465a16c6a4326b
Unknown <teaguefcs@gmail.com>
2018-04-18 10:47:34 -0400

    Added Buttons and Implemented Them
    House button to open doctrine and money menu and background created to close popups.

---
+++ Assets/CultController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CultController : MonoBehaviour {
+
+    public GameObject houseButtonPanel;
+
+	void Start () {
+        houseButtonPanel.SetActive(false);
+	}
+	
+	void Update () {
+		
+	}
+
+    public void ShowHousePanel()
+    {
+        houseButtonPanel.SetActive(true);
+    }
+
+    public void HideAllPanels()
+    {
+        houseButtonPanel.SetActive(false);
+    }
+}
