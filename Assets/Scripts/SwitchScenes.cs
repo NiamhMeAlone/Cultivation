67bd65773a800ddd0b9144fb8b7d6cac83583034
Unknown <mmg551@nyu.edu>
2018-04-30 14:46:21 -0400

    end states for too much of each doctrine and menu made

---
+++ Assets/Scripts/SwitchScenes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScenes : MonoBehaviour {
+    public GameObject harbingerPanel;
+   
+	// Use this for initialization
+	void Start () {
+        harbingerPanel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void ToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void ToMain()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    public void DisplayPanel()
+    {
+        harbingerPanel.SetActive(true);
+    }
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
