eaf4b2acf33bed278456213b092cfa402f561d56
Unknown <mmg551@nyu.edu>
2018-05-06 23:11:14 -0400

    Endings all done and tutorial started

---
+++ Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour {
+    public GameObject[] panels = new GameObject[7]; 
+    int i = 0;
+	// Use this for initialization
+	void Start () {
+        panels[0].SetActive(true);
+        for (int i = 1; i < panels.Length; i++)
+        {
+            panels[i].SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+   
+    public void NextPanel()
+    {
+        panels[i].SetActive(false);
+        panels[i + 1].SetActive(true);
+        i++;
+    }
+               
+
+}
