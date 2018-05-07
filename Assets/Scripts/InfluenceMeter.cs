75a82f7e8915212f6a47b42eb7c019784e5b7383
Unknown <dt1305@nyu.edu>
2018-05-06 21:41:12 -0400

    Updated influence UI in DetScene and added art assets

--- Assets/Scripts/InfluenceMeter.cs
+++ Assets/Scripts/InfluenceMeter.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InfluenceMeter : MonoBehaviour {
 
     public int demoIndex;
     public int maxInfluence;
     private bool locked = false;
+	public Text hoverOverLabel;
+	public Text hoverOverInfluence;
 
 	// Use this for initialization
 	void Start () {
@@ -30,11 +33,23 @@ public class InfluenceMeter : MonoBehaviour {
                 locked = true;
             }
         }
-        transform.localScale = Vector3.one * (((float)CultController.controller.demographics[demoIndex].influence)/((float)maxInfluence));
+		//transform.localScale = Vector3.one - (Vector3.one * (((float)CultController.controller.demographics[demoIndex].influence)/((float)maxInfluence)));
+		GetComponent<Image> ().fillAmount = 1f - (((float)CultController.controller.demographics[demoIndex].influence)/((float)maxInfluence));
+		if (hoverOverInfluence != null) {
+			hoverOverInfluence.text = Mathf.Floor ((((float)CultController.controller.demographics [demoIndex].influence) / ((float)maxInfluence)) * 100f).ToString () + "%";
+		}
 	}
     
     public bool GetLocked()
     {
         return locked;
     }
+	void OnMouseEnter(){
+		hoverOverLabel.color = Color.white;
+		hoverOverInfluence.color = Color.white;
+	}
+	void OnMouseExit(){
+		hoverOverLabel.color = new Color32 (0, 0, 0, 0);
+		hoverOverInfluence.color = new Color32 (0, 0, 0, 0);
+	}
 }
