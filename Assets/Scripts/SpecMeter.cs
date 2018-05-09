4843b3b3821093f4af8123c7a8bdf3c2ff281ea3
Unknown <dt1305@nyu.edu>
2018-05-08 21:10:28 -0400

    Improvement to UI
    Improved doctrine UI.

--- Assets/Scripts/SpecMeter.cs
+++ Assets/Scripts/SpecMeter.cs
@@ -2,36 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SpecMeter : MonoBehaviour
 {
     public int docIndex;
     public int maxSpec;
-    public SpriteRenderer spriteRenderer;
+	public Image meterImage;
 
     // Use this for initialization
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+		meterImage = GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.localScale = new Vector3(1, ((float)CultController.controller.doctrines[docIndex].specialization) / ((float)maxSpec), 1);
-        transform.localPosition = new Vector3(0, -.5f + (transform.localScale.y/2), 0);
-        if (CultController.controller.doctrines[docIndex].specialization > 80)
-        {
-            spriteRenderer.color = new Color(.4f, .9f, .4f);
-        }
-        else if (CultController.controller.doctrines[docIndex].specialization < 20)
-        {
-            spriteRenderer.color = new Color(.9f, .4f, .4f);
-        }
-        else
-        {
-            spriteRenderer.color = new Color(1f, 1f, 1f);
-        }
+        //transform.localScale = new Vector3(1, ((float)CultController.controller.doctrines[docIndex].specialization) / ((float)maxSpec), 1);
+        //transform.localPosition = new Vector3(0, -.5f + (transform.localScale.y/2), 0);
+		meterImage.fillAmount = 1f - ((float)CultController.controller.doctrines[docIndex].specialization / (float)maxSpec);
+//        if (CultController.controller.doctrines[docIndex].specialization > 80)
+//        {
+//            meterImage.color = new Color(.4f, .9f, .4f);
+//        }
+//        else if (CultController.controller.doctrines[docIndex].specialization < 20)
+//        {
+//            meterImage.color = new Color(.9f, .4f, .4f);
+//        }
+//        else
+//        {
+//            meterImage.color = new Color(1f, 1f, 1f);
+//        }
         if ((float)CultController.controller.doctrines[docIndex].specialization >= 100)
         {
             SceneManager.LoadScene("High" + CultController.controller.doctrines[docIndex].name);
